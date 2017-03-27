41ec098478ac47c19cd609fb39ce5de3a46ca68b
Joshua T Corbin <jcorbin@wunjo.org>
2017-03-27 10:42:26 -0700

    Add comments, more prompts, and generally make the tests better

--- qs.go
+++ qs.go
@@ -8,6 +8,7 @@ func (s nums) Len() int           { return len(s) }
 func (s nums) Less(i, j int) bool { return s[i] < s[j] }
 func (s nums) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
+// quicksort sorts a sortable using the quick sort algorithm.
 func quicksort(si sort.Interface) {
 	q := [][2]int{{0, si.Len()}}
 	for len(q) > 0 {
@@ -28,6 +29,7 @@ func quicksort(si sort.Interface) {
 	}
 }
 
+// partition is the core of the quicksort family.
 func partition(si sort.Interface, lo, pi, hi int) int {
 	si.Swap(pi, hi)
 	i := lo
@@ -40,3 +42,19 @@ func partition(si sort.Interface, lo, pi, hi int) int {
 	si.Swap(hi, i)
 	return i
 }
+
+// TODO
+// // quickselect does enough partial sorting to fix the position of the i-th
+// // element in the final sorted order.
+// func quickselect(si sort.Interface, i int)
+
+// TODO
+// // quickselectmany is just quickselect for more than one index in a single
+// // pass.
+// func quickselectmany(si sort.Interface, is ...int)
+
+// TODO
+// // quickselectfirst is like quickselect, except after fixing the given index it
+// // seeks backwards over all values equal to the fixed one (in sorted order),
+// // returning the index of the first occurrence.
+// func quickselectfirst(si sort.Interface, i int) int
--- qs_test.go
+++ qs_test.go
@@ -1,23 +1,28 @@
 package qs
 
 import (
-	"fmt"
 	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var testData = [][]int{
-	{},
-	{42},
-	{85, 74, 43, 83, 31},
-	{
+// NOTE: you can easily change or add testing data using `jot -r N` on a BSD
+// system (TODO GNU equiv?)
+
+var testData = []struct {
+	name string
+	xs   []int
+}{
+	{"empty", []int{}},
+	{"the one", []int{42}},
+	{"rand 5", []int{85, 74, 43, 83, 31}},
+	{"rand 25", []int{
 		6, 76, 4, 79, 21, 87, 15, 43, 29, 98,
 		19, 11, 57, 95, 43, 51, 59, 1, 23, 55,
 		94, 84, 60, 98, 95,
-	},
-	{
+	}},
+	{"rand 100", []int{
 		85, 24, 45, 21, 44, 91, 27, 56, 40, 20,
 		89, 31, 75, 39, 99, 68, 78, 99, 96, 20,
 		74, 98, 57, 49, 19, 6, 1, 89, 66, 46,
@@ -28,17 +33,124 @@ var testData = [][]int{
 		50, 88, 41, 81, 80, 65, 1, 43, 5, 41,
 		58, 99, 61, 42, 84, 92, 76, 69, 25, 42,
 		95, 31, 46, 49, 44, 56, 6, 97, 30, 45,
-	},
+	}},
+	{"tied median", []int{
+		// this sample has a 5-element cohort tied for median
+		12, 13, 18, 2, 23, 27, 29, 29, 33, 33,
+		44, 44, 44, 44, 44, 56, 60, 62, 65, 66,
+		74, 82, 90, 96, 97,
+	}},
 }
 
 func TestQuickSort(t *testing.T) {
-	for _, xs := range testData {
-		t.Run(fmt.Sprintf("len() = %d", len(xs)), func(t *testing.T) {
-			a := append(nums(nil), xs...)
-			b := append(nums(nil), xs...)
-			quicksort(nums(b))
+	for _, tc := range testData {
+		t.Run(tc.name, func(t *testing.T) {
+			// the standard way
+			a := append(nums(nil), tc.xs...)
 			sort.Sort(a)
+
+			// our way
+			b := append(nums(nil), tc.xs...)
+			quicksort(nums(b))
+
+			// same?
 			assert.Equal(t, a, b)
 		})
 	}
 }
+
+// // TODO: uncomment when implementing quickselect
+// func TestQuickSelect(t *testing.T) {
+// 	for _, pcts := range [][]float64{
+// 		{0.5},
+// 		{0.25, 0.5, 0.75},
+// 		{0.25, 0.5, 0.75, 0.9, 0.95, 0.99},
+// 	} {
+// 		for _, tc := range testData {
+// 			t.Run(tc.name, func(t *testing.T) {
+// 				for _, pct := range pcts {
+// 					t.Run(fmt.Sprintf("quickselect %0.2f", pct), func(t *testing.T) {
+// 						i := int(pct * float64(len(tc.xs)))
+
+// 						// the long way round
+// 						a := append(nums(nil), tc.xs...)
+// 						sort.Sort(a)
+
+// 						// QET
+// 						b := append(nums(nil), tc.xs...)
+// 						quickselect(nums(b), i)
+
+// 						// same?
+// 						assert.Equal(t, a[i], b[i])
+// 					})
+// 				}
+// 			})
+// 		}
+// 	}
+// }
+
+// // TODO: uncomment when implementing quickselectmany
+// func TestQuickSelectMany(t *testing.T) {
+// 	for _, pcts := range [][]float64{
+// 		{0.5},
+// 		{0.25, 0.5, 0.75},
+// 		{0.25, 0.5, 0.75, 0.9, 0.95, 0.99},
+// 	} {
+// 		for _, tc := range testData {
+// 			t.Run(tc.name, func(t *testing.T) {
+// 				is := make([]int, len(pcts))
+// 				for i, pct := range pcts {
+// 					is[i] = int(pct * float64(len(tc.xs)))
+// 				}
+
+// 				// the long way round
+// 				a := append(nums(nil), tc.xs...)
+// 				sort.Sort(a)
+// 				avs := make([]int, len(is))
+// 				for i := range is {
+// 					avs[i] = a[is[i]]
+// 				}
+
+// 				// QET
+// 				b := append(nums(nil), tc.xs...)
+// 				quickselectmany(nums(b), is...)
+// 				bvs := make([]int, len(is))
+// 				for i := range is {
+// 					bvs[i] = b[is[i]]
+// 				}
+
+// 				// same?
+// 				assert.Equal(t, avs, bvs)
+// 			})
+// 		}
+// 	}
+// }
+
+// // TODO: uncomment when implementing quickselectfirst
+// func TestQuickSelectFirst(t *testing.T) {
+// 	for _, pct := range []float64{0.25, 0.5, 0.75, 0.9, 0.95, 0.99} {
+// 		for _, tc := range testData {
+// 			t.Run(tc.name, func(t *testing.T) {
+// 				t.Run(fmt.Sprintf("quickselect %0.2f", pct), func(t *testing.T) {
+// 					i := int(pct * float64(len(tc.xs)))
+
+// 					// the long way round
+// 					a := append(nums(nil), tc.xs...)
+// 					sort.Sort(a)
+// 					j := i
+// 					for j > 0 && a[j-1] == a[j] {
+// 						j--
+// 					}
+
+// 					// QET
+// 					b := append(nums(nil), tc.xs...)
+// 					k := quickselectfirst(nums(b), i)
+
+// 					// same?
+// 					assert.Equal(t, j, k)
+// 					assert.Equal(t, a[j], b[k])
+// 				})
+// 			})
+// 		}
+// 	}
+// }
